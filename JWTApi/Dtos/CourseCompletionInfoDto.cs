af05b6847d3af2fbbf7145193e7fb138feae17ab
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-01-06 19:49:50 +0600

    Add files via upload

---
+++ JWTApi/Dtos/CourseCompletionInfoDto.cs
@@ -0,0 +1,15 @@
+namespace JWTApi.Dtos
+{
+    public class CourseCompletionInfoDto
+    {
+        public int totalQuestion { get; set; }
+        public int TotalSeen { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CourseCompletionInfo
+    {
+        public int? ChapterId { get; set; }
+        public int TotalQuestion { get; set; }
+    }
+}
---
+++ JWTApi/Dtos/ProgressInfoDto.cs
@@ -0,0 +1,9 @@
+namespace JWTApi.Dtos
+{
+    public class ProgressInfoDto
+    {
+        public int Quantity { get; set; }
+        public int TotalCorrectAnswer { get; set; }
+        public int TotalWrongAnswer { get; set; }
+    }
+}
