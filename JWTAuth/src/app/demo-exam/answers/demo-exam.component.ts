1a990dceddb712b0431bed9adc499dacb7854221
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-01-06 19:43:20 +0600

    Add files via upload

---
+++ JWTAuth/src/app/demo-exam/answers/demo-exam.component.ts
@@ -0,0 +1,42 @@
+import { Question } from './../_models/question';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '@app/_models/Course';
+import { CourseService } from '@app/_services/course.service';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/_services';
+import { Images } from '@app/_models/images';
+
+@Component({
+  selector: 'app-demo-exam',
+  templateUrl: './demo-exam.component.html',
+  styleUrls: ['./demo-exam.component.less']
+})
+export class DemoExamComponent implements OnInit {
+
+  courses: Course[];
+  questions: Question[];
+  images: Images[];
+  questionGet: boolean = false;
+  selectedCourseCode: number;
+
+  constructor(private courseService: CourseService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+  getCourses(){
+    this.courseService.getCoursesDemo()
+    .subscribe((course: Course[]) => {
+      this.courses = course;
+      console.log(this.courses);
+    });
+  }
+
+  TakeDemo(courseCode: number){
+    this.selectedCourseCode = courseCode;
+    console.log(this.selectedCourseCode);
+  }
+
+}
---
+++ JWTAuth/src/app/demo-exam/answers/demo-question/demo-question.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CourseService } from '@app/_services/course.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Course } from '@app/_models/Course';
+import { Question } from '@app/_models/question';
+
+@Component({
+  selector: 'app-demo-question',
+  templateUrl: './demo-question.component.html',
+  styleUrls: ['./demo-question.component.less']
+})
+export class DemoQuestionComponent implements OnInit {
+
+  courseCode: number;
+  courses: Course[];
+  questions: Question[];
+  
+  constructor(private courseService: CourseService,
+    private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.TakeDemo();
+  }
+
+  TakeDemo(){
+    this.courseService.getDemoQuestions(+this.route.snapshot.params['courseCode']).subscribe((question: Question[]) => {
+      this.questions = question;
+      console.log(this.questions);
+      this.courseService.questions = question;
+    });
+    this.courseService.selectedAnswer = new Array(10);
+  }
+
+  selected(option: string, index: number){
+    this.courseService.selectedAnswer[index]=option;
+    console.log(this.courseService.selectedAnswer[index], index);
+  }
+
+  submit(){
+    this.router.navigate(['/demo/answer']);
+    console.log(this.courseService.selectedAnswer);
+    // console.log(this.courseService.selectedAnswer[7]);
+  }
+
+}
