b83800281906885ebe5713124fc5894b48b31350
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:37:53 +0600

    Update UserForLoginDto.cs

--- JWTApi/Dtos/UserForLoginDto.cs
+++ JWTApi/Dtos/UserForLoginDto.cs
@@ -7,5 +7,6 @@ namespace JWTApi.Dtos
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Token { get; set; }
+        public string Role { get; set; }
     }
 }
