c1fdc6edb7f759786112608e258bb2d65d8138d1
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:29:26 +0600

    Update PerformanceService.cs

--- JWTApi/Data/PerformanceService.cs
+++ JWTApi/Data/PerformanceService.cs
@@ -19,17 +19,17 @@ namespace JWTApi.Data
         {
             var totalQsn =  ((from p in _context.Questions.AsEnumerable()
                       join q in _context.QuestionStatuses.AsEnumerable() on p.QuestionId equals q.QuestionId
-                      where (q.StudentId == studentId && q.CourseCode == courseCode)
+                      where (q.UserId == studentId && q.CourseCode == courseCode)
                       group new { p, q } by new { p.ChapterId } into g
                       select new CourseCompletionInfo { ChapterId = g.Key.ChapterId, TotalQuestion = g.Count() }).ToList());
             
             var totalSeen = ((from p in _context.Questions.AsEnumerable()
                       join q in _context.QuestionStatuses.AsEnumerable() on p.QuestionId equals q.QuestionId
-                      where (q.TSeenOrUnseen == 1 && q.StudentId == studentId && q.CourseCode == courseCode)
+                      where (q.TSeenOrUnseen == 1 && q.UserId == studentId && q.CourseCode == courseCode)
                       group new { p, q } by new { p.ChapterId } into g
                       select new CourseCompletionInfo { ChapterId = g.Key.ChapterId, TotalQuestion = g.Count() }).ToList());
             
-            var courses = _context.Chapters.FromSqlRaw("SELECT DISTINCT Chapters.ChapterId, Chapters.Name, Chapters.CourseCOde FROM Questions, Chapters WHERE Chapters.ChapterId IN(SELECT DISTINCT ChapterId FROM Questions) AND Chapters.CourseCode = {0}", courseCode).ToList();
+            var courses = _context.Chapters.FromSqlRaw("SELECT DISTINCT Chapters.ChapterId, Chapters.Name, Chapters.CourseCOde, Chapters.ImageId FROM Questions, Chapters WHERE Chapters.ChapterId IN(SELECT DISTINCT ChapterId FROM Questions) AND Chapters.CourseCode = {0}", courseCode).ToList();
 
             IEnumerable<CourseCompletionInfoDto> info = new List<CourseCompletionInfoDto>();
             int len = totalSeen.Count();
@@ -61,10 +61,8 @@ namespace JWTApi.Data
         public async Task<IEnumerable<TestExam>> GetProgressInfo(int courseCode, int studentId)
         {
             var info = await _context.TestExams
-                    .FromSqlRaw("SELECT TestExamId, Quantity, TotalCorrectAnswer, TotalWrongAnswer, StudentId, CourseCode FROM TestExams WHERE StudentId = {0} AND CourseCode = {1}", studentId, courseCode).ToListAsync();
+                    .FromSqlRaw("SELECT TestExamId, Quantity, TotalCorrectAnswer, TotalWrongAnswer, UserId, CourseCode FROM TestExams WHERE UserId = {0} AND CourseCode = {1}", studentId, courseCode).ToListAsync();
             return info;
         }
     }
 }
-
-// https://www.youtube.com/watch?v=dRVdxlwxIK0
