1b77fad7ee2f874f19baef5d7a80d89942f146b6
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:48:42 +0600

    Add files via upload

---
+++ JWTApi/Models/User.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace JWTApi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public ICollection<PracticeExam> PracticeExam { get; set; }
+        public ICollection<QuestionStatus> QuestionStatuses { get; set; }
+        public ICollection<Subscription> Subscriptions { get; set; }
+        public ICollection<TestExam> TestExams { get; set; }
+    }
+}
