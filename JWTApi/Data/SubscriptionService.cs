f5e0812e96449698649de3cd05e67e4105cfb655
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:29:51 +0600

    Update SubscriptionService.cs

--- JWTApi/Data/SubscriptionService.cs
+++ JWTApi/Data/SubscriptionService.cs
@@ -22,24 +22,24 @@ namespace JWTApi.Data
             var update = _context.Subscriptions.Where(w => w.Length <= DateTime.Now).ToList();
             foreach(var i in update)
             {
-                removeStatus(i.StudentId.Value, i.CourseCode.Value);
+                removeStatus(i.UserId.Value, i.CourseCode.Value);
                 _context.Remove(i);
             }
             _context.SaveChanges();
             var courses = await _context.CourseModel.
-            FromSqlRaw("SELECT Courses.Name, Subscriptions.Length, Courses.CourseCode FROM Courses, Subscriptions WHERE Courses.CourseCode=Subscriptions.CourseCode AND Subscriptions.StudentId={0}",studentId).ToListAsync(); 
+            FromSqlRaw("SELECT Courses.Name, Subscriptions.Length, Courses.CourseCode, Images.ImageUrl FROM Courses, Subscriptions, Images WHERE Courses.CourseCode=Subscriptions.CourseCode AND Courses.ImageId=Images.ImageId AND Subscriptions.UserId={0}",studentId).ToListAsync(); 
             _context.SaveChanges();
             return courses;
         }
 
         void removeStatus(int studentId, int CourseCode)
         {
-           var question = _context.QuestionModel.FromSqlRaw("SELECT ChapterId, QuestionId, question, Option1, Option2, Option3, Option4, CorrectAnswer, AnswerDetails FROM Questions WHERE ChapterId IN(SELECT ChapterId FROM Chapters WHERE CourseCode={0})", CourseCode).ToList();        
+            var question = _context.QuestionModel.FromSqlRaw("SELECT ChapterId, QuestionId, question, Option1, Option2, Option3, Option4, CorrectAnswer, AnswerDetails FROM Questions WHERE ChapterId IN(SELECT ChapterId FROM Chapters WHERE CourseCode={0})", CourseCode).ToList();        
             foreach (var i in question)
             {
                 // db.People.RemoveRange(db.People.Where(x => x.State == "CA"));
                 _context.QuestionStatuses.RemoveRange
-                (_context.QuestionStatuses.Where(x => x.StudentId == studentId && x.QuestionId == i.QuestionId));
+                (_context.QuestionStatuses.Where(x => x.UserId == studentId && x.QuestionId == i.QuestionId));
             }
         }
 
@@ -51,7 +51,7 @@ namespace JWTApi.Data
                 Length = DateTime.Now.AddMonths(subscriptionDto.Length),
                 // Length = DateTime.Now.AddMinutes(2),
                 // Length = DateTime.UtcNow.Date,
-                StudentId = subscriptionDto.StudentId,
+                UserId = subscriptionDto.StudentId,
                 CourseCode = subscriptionDto.CourseCode
             };
             _context.Subscriptions.Add(sub);
@@ -72,8 +72,9 @@ namespace JWTApi.Data
             {
                     var z = new QuestionStatus()
                     {
-                        StudentId = studentId,
+                        UserId = studentId,
                         QuestionId = i.QuestionId,
+                        CourseCode = CourseCode,
                         PSeenOrUnseen=0,
                         TSeenOrUnseen = 0,
                         PCorrectOrWrong = 0,
