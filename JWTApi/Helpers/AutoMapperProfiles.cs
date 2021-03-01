554fcf8454b08828c8b6969f278b1614b22c0d5c
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:40:42 +0600

    Update AutoMapperProfiles.cs

--- JWTApi/Helpers/AutoMapperProfiles.cs
+++ JWTApi/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,7 @@ namespace JWTApi.Helpers
             CreateMap<Image, ImagesDto>();
             CreateMap<Question, AnswerDto>();
             CreateMap<Question, UploadQuestionDto>();
+            CreateMap<TestExam, ProgressInfoDto>();
         }
     }
 }
