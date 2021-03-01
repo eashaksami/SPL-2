77bbe784642c309dff443050f95a82e4708c7fbf
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:47:04 +0600

    Update Subscription.cs

--- JWTApi/Models/Subscription.cs
+++ JWTApi/Models/Subscription.cs
@@ -10,9 +10,9 @@ namespace JWTApi.Models
         public int SubscriberId { get; set; }
         public int Price { get; set; }
         public DateTime Length { get; set; }
-        public int? StudentId { get; set; }
-        [ForeignKey("StudentId")]
-        public Student Student { get; set; }
+        public int? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
         public int? CourseCode { get; set; }
         [ForeignKey("CourseCode")]
         public Course Course { get; set; }
