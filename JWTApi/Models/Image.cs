4faf14e6203531609a320a059111dafe04067a6e
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-01 19:44:04 +0600

    Update Image.cs

--- JWTApi/Models/Image.cs
+++ JWTApi/Models/Image.cs
@@ -7,5 +7,7 @@ namespace JWTApi.Models
         public int ImageId { get; set; }
         public string ImageUrl { get; set; }
         public ICollection<Question> Questions { get; set; }
+        public ICollection<Course> Courses { get; set; }
+        public ICollection<Chapter> Chapters { get; set; }
     }
 }
