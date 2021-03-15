dd9d02d5a65e632c151e463da117114f2162876b
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-15 21:13:41 +0600

    Update app.component.ts

--- JWTAuth/src/app/app.component.ts
+++ JWTAuth/src/app/app.component.ts
@@ -1,12 +1,15 @@
-﻿import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from './_services';
 import { User } from './_models';
 
-@Component({ selector: 'app', templateUrl: 'app.component.html' })
-export class AppComponent {
+@Component({ selector: 'app',
+             templateUrl: 'app.component.html',
+             styleUrls: ['app.component.css']})
+export class AppComponent implements OnInit{
     currentUser: User;
+    isLoggedIn: boolean;
 
     constructor(
         private router: Router,
@@ -14,9 +17,21 @@ export class AppComponent {
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     }
+    ngOnInit(): void {
+        var user = this.authenticationService.getUsername();
+        if(user==null)
+        {
+            this.isLoggedIn = false;
+        }
+        else
+        {
+            this.isLoggedIn = true;
+        }
+    }
 
     logout() {
         this.authenticationService.logout();
-        this.router.navigate(['/login']);
+        this.isLoggedIn = false;
+        this.router.navigate(['']);
     }
 }
