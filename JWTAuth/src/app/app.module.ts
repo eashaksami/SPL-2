fc97ac49646524a464d53e152cf85e868a1fbfc3
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-03-15 21:13:19 +0600

    Update app.module.ts

--- JWTAuth/src/app/app.module.ts
+++ JWTAuth/src/app/app.module.ts
@@ -1,4 +1,5 @@
-﻿import { SubscriptionsService } from './_services/subscriptions.service';
+import { PerformanceService } from './_services/performance.service';
+import { SubscriptionsService } from './_services/subscriptions.service';
 import { AnswerComponent } from './question/answer/answer.component';
 import { CourseService } from './_services/course.service';
 import { NgModule } from '@angular/core';
@@ -21,14 +22,20 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { ChapterComponent } from './chapter/chapter.component';
 import { CourseComponent } from './course/course.component';
 import { QuestionComponent } from './question/question.component'
-import { CommonModule, DatePipe } from '@angular/common';import { SubscriptionComponent } from './course/subscription/subscription.component';;
+import { CommonModule } from '@angular/common';
+import { SubscriptionComponent } from './course/subscription/subscription.component';;
 import { MySubscriptionsComponent } from './my-subscriptions/my-subscriptions.component';
 import { DemoExamComponent } from './demo-exam/demo-exam.component';
 import { AnswersComponent } from './demo-exam/answers/answers.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { PracticeExamComponent } from './practice-exam/practice-exam.component';
 import { DemoQuestionComponent } from './demo-exam/demo-question/demo-question.component';
-import { ChartJsComponent } from './chart-js/chart-js.component';
+import { ChartJsComponent } from './chart-js/chart-js.component';;
+import { ProgressGraphComponent } from './progress-graph/progress-graph.component';
+import { CourseCompletionComponent } from './course-completion/course-completion.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
 @NgModule({
     imports: [
         BrowserModule,
@@ -56,13 +63,18 @@ import { ChartJsComponent } from './chart-js/chart-js.component';
         ImageUploadComponent ,
         PracticeExamComponent ,
         DemoQuestionComponent,
-        ChartJsComponent    
+        ChartJsComponent ,
+        ProgressGraphComponent ,
+        CourseCompletionComponent ,
+        LoadingSpinnerComponent ,
+        HomeComponentComponent
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         CourseService,
-        SubscriptionsService
+        SubscriptionsService,
+        PerformanceService
 
         // provider used to create fake backend
         //fakeBackendProvider
