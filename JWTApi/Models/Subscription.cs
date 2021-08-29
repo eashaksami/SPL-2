88adb15fcfcc7811fb9a05f7a70606a5d2297d38
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 02:30:13 +0600

    Update Subscription.cs

--- JWTApi/Models/Subscription.cs
+++ JWTApi/Models/Subscription.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace JWTApi.Models
+namespace EBET.Models
 {
     public class Subscription
     {
