e03a29e89cd86ef727ed3100636bdcb1ebacd075
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 02:12:25 +0600

    Update IAdminService.cs

--- JWTApi/Data/IAdminService.cs
+++ JWTApi/Data/IAdminService.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using JWTApi.Dtos;
+using EBET.Dtos;
+using EBET.Models;
+using Microsoft.AspNetCore.Http;
 
-namespace JWTApi.Data
+namespace EBET.Data
 {
     public interface IAdminService
     {
         Task<UploadQuestionDto> UploadQuestion(UploadQuestionDto uploadQuestionDto);
         Task<IEnumerable<GetQuestionDto>> ViewQuestions(int chapterId);
+        Task<Question> UpdateQuestion(GetQuestionDto questionDto);
+        Task<IEnumerable<UserForLoginDto>> ViewAllStudents();
+        Course addNewCourse(string courseName, int imageId);
+        Image uploadImage(IFormFile file);
+        Task<Question> DeleteQuestion(int questionId);
     }
 }
