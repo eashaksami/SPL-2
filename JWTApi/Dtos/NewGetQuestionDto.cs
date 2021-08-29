38a26420028ca015572b41b89b2140966bcae935
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 02:20:29 +0600

    Add files via upload

---
+++ JWTApi/Dtos/NewGetQuestionDto.cs
@@ -0,0 +1,15 @@
+namespace EBET.Dtos
+{
+    public class NewGetQuestionDto
+    {
+        public int QuestionId { get; set; }
+        public int ChapterId { get; set; }
+        public string question { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string AnswerDetails { get; set; }
+    }
+}
