63d24de7eea84476cd4138d41d0f7b923673ac32
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 02:28:27 +0600

    Update Course.cs

--- JWTApi/Models/Course.cs
+++ JWTApi/Models/Course.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace JWTApi.Models
+namespace EBET.Models
 {
     public class Course
     {
