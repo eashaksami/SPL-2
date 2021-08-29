1e3a8de9e8966bcda8c7b6898818ae4c7f97f03b
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 02:23:39 +0600

    Update UserForLoginDto.cs

--- JWTApi/Dtos/UserForLoginDto.cs
+++ JWTApi/Dtos/UserForLoginDto.cs
@@ -1,8 +1,8 @@
-namespace JWTApi.Dtos
+namespace EBET.Dtos
 {
     public class UserForLoginDto
     {
-        public int StudentId { get; set; }
+        public int UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
