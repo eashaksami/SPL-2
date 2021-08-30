c0b3300b2d2d5862b6cc7d547ac305f41f6d2f78
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 10:19:54 +0600

    Update question.ts

--- JWTAuth/src/app/_models/question.ts
+++ JWTAuth/src/app/_models/question.ts
@@ -1,9 +1,15 @@
 export interface Question{
     questionId: number;
+    chapterId: number;
+    imageId: number;
+    imageUrl: string;
+    imageIdForAnswer: number;
+    imageUrlForAnswer: string;
     question: string;
     option1: string;
     option2: string;
     option3: string;
     option4: string;
     correctAnswer: string;
+    answerDetails: string;
 }
