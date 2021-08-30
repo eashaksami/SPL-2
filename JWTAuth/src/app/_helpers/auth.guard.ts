93c8c78ba85f39ff73d33c61290f93d5235fd061
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-08-30 10:14:27 +0600

    Update auth.guard.ts

--- JWTAuth/src/app/_helpers/auth.guard.ts
+++ JWTAuth/src/app/_helpers/auth.guard.ts
@@ -1,5 +1,6 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 import { AuthenticationService } from '@app/_services';
 
@@ -12,8 +13,19 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const currentUser = this.authenticationService.currentUserValue;
+        
+
         if (currentUser) {
             // logged in so return true
+            // return true;
+            let roles = route.data['permittedRoles'] as Array<string>;
+            if(roles){
+            if(this.authenticationService.roleMatch(roles)) return true;
+            else{
+                this.router.navigate(['/forbidden']);
+                return false;
+            }
+            }
             return true;
         }
 
