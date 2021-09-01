a1061e705b00fda079eead5fc796d09754debc92
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-09-01 21:53:52 +0600

    Update app.module.ts

--- JWTAuth/src/app/app.module.ts
+++ JWTAuth/src/app/app.module.ts
@@ -9,15 +9,13 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CountdownModule } from 'ngx-countdown';
 import { ChartsModule } from 'ng2-charts';
 
-// used to create fake backend
-//import { fakeBackendProvider } from './_helpers';
 
 import { AppComponent } from './app.component';
 import { appRoutingModule } from './app.routing';
 
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
-import { LoginComponent } from './login';;
+import { LoginComponent } from './login';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ChapterComponent } from './chapter/chapter.component';
 import { CourseComponent } from './course/course.component';
@@ -30,12 +28,21 @@ import { AnswersComponent } from './demo-exam/answers/answers.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { PracticeExamComponent } from './practice-exam/practice-exam.component';
 import { DemoQuestionComponent } from './demo-exam/demo-question/demo-question.component';
-import { ChartJsComponent } from './chart-js/chart-js.component';;
+import { ChartJsComponent } from './chart-js/chart-js.component';
 import { ProgressGraphComponent } from './progress-graph/progress-graph.component';
 import { CourseCompletionComponent } from './course-completion/course-completion.component';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
-
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { UploadQuestionComponent } from './upload-question/upload-question.component';
+import { ViewQuestionComponent } from './view-question/view-question.component';
+import { ViewStudentComponent } from './view-student/view-student.component';
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { AdminService } from './_services/admin.service';
+import { UpdateCourseComponent } from './update-course/update-course.component';
+import { AddQuestionComponent } from './update-course/add-question/add-question.component';
+import { ViewQuestionsComponent } from './update-course/view-questions/view-questions.component';
 @NgModule({
     imports: [
         BrowserModule,
@@ -67,14 +74,25 @@ import { HomeComponentComponent } from './home-component/home-component.componen
         ProgressGraphComponent ,
         CourseCompletionComponent ,
         LoadingSpinnerComponent ,
-        HomeComponentComponent
-    ],
+        HomeComponentComponent,
+        ForbiddenComponent ,
+        UploadQuestionComponent ,
+        ViewQuestionComponent ,
+        ViewStudentComponent ,
+        AdminHomePageComponent,
+        AddCourseComponent
+,
+        UpdateCourseComponent ,
+        AddQuestionComponent ,
+        ViewQuestionsComponent  ],
+        
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         CourseService,
         SubscriptionsService,
-        PerformanceService
+        PerformanceService,
+        AdminService
 
         // provider used to create fake backend
         //fakeBackendProvider
