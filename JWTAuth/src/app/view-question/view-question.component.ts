281206fd7703e927d45c889d208e57ba3bccbb74
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-09-01 21:51:22 +0600

    Add files via upload

---
+++ JWTAuth/src/app/view-question/view-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-question',
+  templateUrl: './view-question.component.html',
+  styleUrls: ['./view-question.component.less']
+})
+export class ViewQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
